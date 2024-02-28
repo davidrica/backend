865e4a1f847f2d449c939e33676e9f39eac412da
davidrica <torresdavidricardo.red@gmail.com>
2024-02-27 21:13:56 -0300

    test ip

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -2,6 +2,8 @@ const {response} = require('express')
 const jwt = require('jsonwebtoken');
 
 const validarJWT = (req,res=response,next)=>{
+    
+    console.log(req.socket.remoteAddress)
     const token = req.header('x-token');
     if(!token){
         return res.status(401).json({
